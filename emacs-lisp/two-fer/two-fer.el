b2ce89e09e45daaaf5506a754487e4061d6023a9
Helder Burato Berto <helder.burato@gmail.com>
2020-08-22 10:15:29 -0300

    emacs-lisp: Solve exercise "Two Fer"

---
+++ emacs-lisp/two-fer/two-fer.el
@@ -0,0 +1,11 @@
+;;; two-fer.el --- Two-fer Exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+(defun two-fer (&optional name)
+  (let ((name (or name "you")))
+    (format "One for %s, one for me." name)))
+
+(provide 'two-fer)
+;;; two-fer.el ends here
